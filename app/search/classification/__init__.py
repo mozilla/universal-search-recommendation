c3f16921d9160045de40045dbdcab10abe5dbd2f
Chuck Harmston <chuck@chuckharmston.com>
2015-12-30 10:49:04 -0600

    Adds Embedly enhancer (fixes #8).

--- app/search/classification/__init__.py
+++ app/search/classification/__init__.py
@@ -1,8 +1,10 @@
 from app.search.classification.domain import DomainClassifier
+from app.search.classification.embedly import EmbedlyClassifier
 from app.search.classification.wikipedia import WikipediaClassifier
 
 
 CLASSIFIERS = [
     DomainClassifier,
+    EmbedlyClassifier,
     WikipediaClassifier,
 ]
